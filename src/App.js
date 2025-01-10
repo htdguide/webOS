628eddad2e17ba994555592fa27b34cfeb117526
htdguide <115078044+htdguide@users.noreply.github.com>
2025-01-11 00:10:17 +1300

    Getting rid of the error message in telegram messenger

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button } from 'react-bootstrap';
+import "./App.css";
 
 const script = document.createElement('script');
 var isCanvasClosed = false;
