deae935067e17b55f2b3d77222776cb394f32283
htdguide <115078044+htdguide@users.noreply.github.com>
2025-02-16 22:11:51 +1300

    Removed

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(() => {
-  // Load environment variables from process.env
-  const key = process.env.SSL_PRIVATE_KEY;
-  const cert = process.env.SSL_CERT_CHAIN;
+// https://vite.dev/config/
+export default defineConfig({
 
-  // Create temporary files for key and cert if they exist
-  const keyFile = key ? './ssl-key.pem' : '';
-  const certFile = cert ? './ssl-cert.pem' : '';
-
-  if (key && cert) {
-    fs.writeFileSync(keyFile, key);
-    fs.writeFileSync(certFile, cert);
-  }
-
-  return {
-    server: {
-      https: key && cert ? { key: fs.readFileSync(keyFile), cert: fs.readFileSync(certFile) } : false
-    },
-    plugins: [react()]
-  };
-});
+  plugins: [react()],
+})
