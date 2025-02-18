9138d8ff2a395b3ac5818eca73b7ba08b49a8d62
htdguide <115078044+htdguide@users.noreply.github.com>
2025-02-18 21:29:18 +1300

    Added time in the menu bar #14

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MenuBar.css';
 
 function MenuBar() {
+  const [currentTime, setCurrentTime] = useState(getFormattedTime());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(getFormattedTime());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function getFormattedTime() {
+    const options = { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+    let time = new Date().toLocaleString('en-US', options);
+    return time.replace(/,/g, '').replace(' AM', 'AM').replace(' PM', 'PM');
+  }
+
   return (
     <div className="menu-bar">
       <div className="menu-left">
@@ -11,6 +27,7 @@ function MenuBar() {
       </div>
       <div className="menu-right">
         <span className="menu-username">htdguide</span>
+        <span className="menu-time">{currentTime}</span>
       </div>
     </div>
   );
