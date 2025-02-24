48e7b571a08505741386566a2afbaf4fafb012ce
htdguide <115078044+htdguide@users.noreply.github.com>
2025-02-25 11:14:02 +1300

    Navbar styling improved

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -2,20 +2,31 @@ import React, { useState, useEffect } from 'react';
 import './MenuBar.css';
 
 function MenuBar() {
-  const [currentTime, setCurrentTime] = useState(getFormattedTime());
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentTime(getFormattedTime());
+      setCurrentTime(new Date());
     }, 1000);
 
     return () => clearInterval(timer);
   }, []);
 
-  function getFormattedTime() {
-    const options = { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
-    let time = new Date().toLocaleString('en-US', options);
-    return time.replace(/,/g, '').replace(' AM', 'AM').replace(' PM', 'PM');
+  function renderFormattedTime(date) {
+    const weekday = date.toLocaleDateString('en-US', { weekday: 'short' });
+    const day = date.getDate();
+    const month = date.toLocaleDateString('en-US', { month: 'short' });
+    const timeStr = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const [hm, ampm] = timeStr.split(' ');
+    const [hour, minute] = hm.split(':');
+    return (
+      <span>
+        {weekday} {day} {month}&nbsp;&nbsp;&nbsp;
+        <span className="time-hour">{hour}</span>
+        <span className="time-colon">:</span>
+        <span className="time-minute">{minute}</span> {ampm}
+      </span>
+    );
   }
 
   return (
@@ -27,7 +38,7 @@ function MenuBar() {
       </div>
       <div className="menu-right">
         <span className="menu-username">htdguide</span>
-        <span className="menu-time">{currentTime}</span>
+        <span className="menu-time">{renderFormattedTime(currentTime)}</span>
       </div>
     </div>
   );
