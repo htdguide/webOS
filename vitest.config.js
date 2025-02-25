3c2994b6c717db865b75d31aa56a64bd3054cf4a
htdguide <115078044+htdguide@users.noreply.github.com>
2025-02-25 14:43:47 +1300

    First working test ready!

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    test: {
-        environment: "jsdom",
-    },
+  test: {
+    globals: true,   // <-- enable global expect, describe, it, etc.
+    environment: "jsdom",
+    // optional but recommended to keep setup code in a single file:
+    // setupFiles: ['./setupTests.js']
+  },
 });
