63012e42628f6ac5594935329479ac09f6324056
htdguide <115078044+htdguide@users.noreply.github.com>
2025-08-05 16:15:17 +1200

    css

--- src/webOS/WebOS.jsx
+++ src/webOS/WebOS.jsx
@@ -15,7 +15,7 @@
 import React, { useState, useEffect } from 'react';
 import System from './coreservices/System/System.jsx';
 import MissionControl from './coreservices/MissionControl/MissionControl.jsx';
-import "./webOS.css";
+import "./WebOS.css";
 
 ////////////////////
 // Area 2: WebOS component definition
